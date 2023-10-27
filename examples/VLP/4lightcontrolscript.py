4dfb4ae8494f5c0dce5386ab53326d0d73533699
Chris <onwuchekwan@gmail.com>
2023-10-27 14:12:30 -0400

    Working Vlp Control Script with mode, frequency and amplitude control.

--- examples/VLP/4lightcontrolscript.py
+++ examples/VLP/4lightcontrolscript.py
@@ -1,49 +1,69 @@
 from xmlrpc.client import ServerProxy
 import time
+import logging
+
+
+
+#Initialize server proxy
+STA_TX1control = ServerProxy('http://localhost:8080')
 
 # Time Delay in Seconds
-time_delay = 1
-
-# Initialize amplitude and frequency settings as a dictionary
-# where keys are signal numbers and values are the corresponding settings
-amplitude_settings = {
-    1: [1, 2, 3],
-    2: [4, 5, 6],
-    3: [7, 8, 9],
-    # Add more signal settings if needed
-}
-
-frequency_settings = {
-    1: [100, 200, 300],
-    2: [400, 500, 600],
-    3: [700, 800, 900],
-    # Add more signal settings if needed
-}
-#Selector Block
-# 0 = Single, 1 = Double.
-select_configs = [0, 1, 0, 1]  # 1-2 use select1, 3-4 use select2, etc.
-#Initialize server proxy
-STA_TX1control = ServerProxy('http://10.1.1.2:8080')
+time_delay = 2
 
 
-# Loop through each selection block configuration
-for j, select_config in enumerate(select_configs):
-    block = j + 1
-    print(f"[Current Select Configuration for block {block}: {select_config}]")
-    getattr(STA_TX1control, f'set_select{block}')(select_config)
 
-# Loop to set amplitude and frequency
-for i in range(1, 9):  # Assuming 8 signals
-    # If there are amplitude settings for this signal, apply them
-    if i in amplitude_settings:
-        for amp in amplitude_settings[i]:
-            print(f"[Current TX Amplitude Settings for channel {i}: {amp}]")
-            getattr(STA_TX1control, f'set_amp_{i}')(amp)
-            time.sleep(time_delay)
+
+
+def set_amplitude(channel, amplitude):
+    print(f"[Current TX Amplitude Settings for channel {channel}: {amplitude}]")
+    getattr(STA_TX1control, f'set_amp_{channel}')(amplitude)
+
+def set_frequency(channel, frequency):
+    print(f"[Current TX Frequency Settings for channel {channel}: {frequency}]")
+    getattr(STA_TX1control, f'set_freq_{channel}')(frequency)
+
+def set_selection_block(block, select_config):
+        print(f"[Sending Select Configuration for block {block}: {select_config}]")
+        result = getattr(STA_TX1control, f'set_select{block}')(select_config)
+        
+
+def main(amplitude_settings, frequency_settings, select_configs):
+    for i in range(1, len(amplitude_settings) * 2 + 1, 2): 
+        select_idx = (i + 1) // 2
+        select_config = select_configs[select_idx - 1]
+        
+        # Set the selection mode first
+        set_selection_block(select_idx, select_config)
+
+        # Then set amplitude and frequency
+        for (amp1, freq1), (amp2, freq2) in zip(zip(amplitude_settings.get(i, []), frequency_settings.get(i, [])), zip(amplitude_settings.get(i + 1, []), frequency_settings.get(i + 1, []))):
+            if select_config == 1:  # If in double tone
+                set_amplitude(i, amp1)
+                set_frequency(i, freq1)
+                time.sleep(time_delay)  # You forgot to call time.sleep(time_delay) in the original code
+                set_amplitude(i + 1, amp2)
+                set_frequency(i + 1, freq2)
+            else:  # If in single tone
+                set_amplitude(i, amp1)
+                set_frequency(i, freq1)
+
+if __name__ == "__main__":
     
-    # If there are frequency settings for this signal, apply them
-    if i in frequency_settings:
-        for freq in frequency_settings[i]:
-            print(f"[Current TX Frequency Settings for channel {i}: {freq}]")
-            getattr(STA_TX1control, f'set_freq_{i}')(freq)
-            time.sleep(time_delay)
+
+    amplitude_settings = {  
+        1: [0.55],
+        2: [0.25],
+        # Add more signals/channels as needed
+    }
+
+    frequency_settings = {  
+        1: [1000000],
+        2: [1200000],
+        # Add more signals/channels as needed
+    }
+    
+    select_configs = [0,0,0,0]  
+    
+    main(amplitude_settings, frequency_settings, select_configs)
+
+
